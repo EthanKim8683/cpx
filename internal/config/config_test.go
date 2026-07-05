6d17e5a79c483e4d3b2907019de7948809c09c99
Ethan Kim <ethankim8683@gmail.com>
2026-07-05 05:19:21 -0700

    Trim down env vars

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -9,7 +9,6 @@ import (
 func TestLoad(t *testing.T) {
 	t.Setenv("GCC", "/usr/bin/gcc")
 	t.Setenv("CLANG", "/usr/bin/clang")
-	t.Setenv("CLANG_TBLGEN", "/usr/bin/clang-tblgen")
 
 	_, err := Load()
 	require.NoError(t, err)
